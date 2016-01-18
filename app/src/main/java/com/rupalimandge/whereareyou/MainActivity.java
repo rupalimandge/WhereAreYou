91a5c62b0b96b1b8d94f2dc361bf327a92130e62
Rupali Mandge <rupali.mandge@gmail.com>
2016-01-18 18:02:38 +0530

    Push Notifications Added.

--- app/src/main/java/com/rupalimandge/whereareyou/MainActivity.java
+++ app/src/main/java/com/rupalimandge/whereareyou/MainActivity.java
@@ -1,17 +1,105 @@
 package com.rupalimandge.whereareyou;
 
-import android.support.v7.app.ActionBarActivity;
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+import com.parse.ParseQuery;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    Button btnSend = null;
+    EditText etMyMessage = null;
+    static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSend = (Button) findViewById(R.id.btnSend);
+        etMyMessage = (EditText) findViewById(R.id.etMyMessage);
+
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+
+        if (currentapiVersion >= 23) {
+
+            // Assume thisActivity is the current activity
+            int permissionCheck = ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_PHONE_STATE);
+
+            // Here, thisActivity is the current activity
+            if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_PHONE_STATE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_PHONE_STATE)) {
+
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_PHONE_STATE},
+                            MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }
+
+
+        }else{
+            TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+            ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+            installation.put("deviceId",mngr.getDeviceId());
+            installation.saveInBackground();
+        }
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String strMessage = etMyMessage.getText().toString();
+
+              /*  ParsePush push = new ParsePush();
+                String message = strMessage;
+                // push.setChannel("WhereAreYou");
+                push.setMessage(message);
+                push.sendInBackground();*/
+
+
+                ParseQuery query = ParseInstallation.getQuery();
+                query.whereEqualTo("deviceId", "357478060184543");
+                ParsePush push = new ParsePush();
+                push.setQuery(query);
+                push.setMessage(strMessage);
+                push.sendInBackground();
+
+            }
+        });
     }
 
     @Override
@@ -34,5 +122,38 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+                    ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+                    installation.put("deviceId",mngr.getDeviceId());
+                    installation.saveInBackground();
+
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "Permission Denied..!!", Toast.LENGTH_LONG).show();
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
---
+++ app/src/main/java/com/rupalimandge/whereareyou/Receiver.java
@@ -0,0 +1,22 @@
+package com.rupalimandge.whereareyou;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.parse.ParsePushBroadcastReceiver;
+
+/**
+ * Created by Rupali on 18/01/16.
+ */
+public class Receiver extends ParsePushBroadcastReceiver {
+
+    @Override
+    public void onPushOpen(Context context, Intent intent) {
+        Log.e("Push", "Clicked");
+        Intent i = new Intent(context, MainActivity.class);
+        i.putExtras(intent.getExtras());
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        context.startActivity(i);
+    }
+}
