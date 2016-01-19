4e4ca7fbe4807ed484a4e415b582d6de6bc0b16b
krishnakhandagale <khandagalekrishna8@gmail.com>
2016-01-19 15:37:17 +0530

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/misc.xml
    	.idea/modules.xml
    	app/app.iml
    	app/build.gradle
    	app/src/main/AndroidManifest.xml
    	app/src/main/java/com/rupalimandge/whereareyou/MainActivity.java
    	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/rupalimandge/whereareyou/GoogleMapLocationActivity.java
+++ app/src/main/java/com/rupalimandge/whereareyou/GoogleMapLocationActivity.java
@@ -1,16 +1,25 @@
 package com.rupalimandge.whereareyou;
 
+import android.*;
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -20,24 +29,166 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.ParseInstallation;
+import com.parse.ParsePush;
+import com.parse.ParseQuery;
 
 
 public class GoogleMapLocationActivity extends AppCompatActivity implements OnMapReadyCallback,LocationListener {
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_LOCATION = 99;
     private LocationManager locationManager;
     private GoogleMap googleMap;
     private static final long MIN_TIME = 400;
     private static final float MIN_DISTANCE = 1000;
+
+    Button btnSend = null;
+    static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    String deviceId = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_location);
 
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this); //You can also use LocationManager.GPS_PROVIDER and LocationManager.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        btnSend = (Button) findViewById(R.id.btnWhereAreYou);
+
+        // this code will work for receiver. receiver will get the device id(the unique identifer of device) and will update the current location.
+        if(this.getIntent().getExtras() != null){
+            deviceId = this.getIntent().getStringExtra("DeviceId");
+            btnSend.setText("Share Location");
+        }
+
+
+
+        int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+
+        if (currentapiVersion >= 23) {
+
+            // Assume thisActivity is the current activity
+            int permissionCheck = ContextCompat.checkSelfPermission(this,
+                    android.Manifest.permission.READ_PHONE_STATE);
+
+            // Here, thisActivity is the current activity
+            /*if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_ACCESS_LOCATION);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }else*/ if ( ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_ACCESS_LOCATION);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }else if (ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.READ_PHONE_STATE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                // Should we show an explanation?
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                        Manifest.permission.READ_PHONE_STATE)) {
+
+                    // Show an expanation to the user *asynchronously* -- don't block
+                    // this thread waiting for the user's response! After the user
+                    // sees the explanation, try again to request the permission.
+
+                } else {
+
+                    // No explanation needed, we can request the permission.
+
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.READ_PHONE_STATE},
+                            MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+
+                    // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                    // app-defined int constant. The callback method gets the
+                    // result of the request.
+                }
+            }
+
+
+        }else{
+            locationManager= (LocationManager) getSystemService(LOCATION_SERVICE);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
+            //You can also use LocationManager.GPS_PROVIDER and LocationManager.
+            SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+            TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+            ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+            installation.put("deviceId",mngr.getDeviceId());
+            installation.put("name", "Krishna Khandagale");
+            installation.saveInBackground();
+        }
+
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+              /*  ParsePush push = new ParsePush();
+                String message = strMessage;
+                // push.setChannel("WhereAreYou");
+                push.setMessage(message);
+                push.sendInBackground();*/
+
+                ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+
+                ParseQuery query = ParseInstallation.getQuery();
+                query.whereEqualTo("deviceId", "357478060184543");
+                ParsePush push = new ParsePush();
+                push.setQuery(query);
+                String strMessage = null, strPrefix = ":";
+
+                // deviceId:name wants to know....
+                strMessage = installation.get("deviceId").toString().concat(strPrefix.concat(installation.get("name").toString().concat(" wants to know your current location.")));
+
+                push.setMessage(strMessage);
+                push.sendInBackground();
+
+            }
+        });
     }
 
     @Override
@@ -114,5 +265,57 @@ public class GoogleMapLocationActivity extends AppCompatActivity implements OnMa
         dialog.show();
 
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_LOCATION: {
+
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    //You can also use LocationManager.GPS_PROVIDER and LocationManager.
+                    locationManager= (LocationManager) getSystemService(LOCATION_SERVICE);
+                    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                            .findFragmentById(R.id.map);
+                    mapFragment.getMapAsync(this);
+                    //Granted permission
+
+                } else {
+                    // permission denied, boo!
+                    Toast.makeText(getApplicationContext(), "Permission Denied..!!", Toast.LENGTH_LONG).show();
+
+
+                }
+                return;
+            }
+
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+
+                    TelephonyManager mngr = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+
+                    ParseInstallation installation = ParseInstallation.getCurrentInstallation();
+                    installation.put("deviceId",mngr.getDeviceId());
+                    installation.put("name","Krishna Khandagale");
+                    installation.saveInBackground();
+
+                } else {
+
+                    Toast.makeText(getApplicationContext(), "Permission Denied..!!", Toast.LENGTH_LONG).show();
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
     }
 }
