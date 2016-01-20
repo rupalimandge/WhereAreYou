e4bd46a71d1e6284b86e86ae224a5db7994f481e
krishnakhandagale <khandagalekrishna8@gmail.com>
2016-01-20 18:22:27 +0530

    PermissionUtilAdded

--- app/src/main/java/com/rupalimandge/whereareyou/PermissionUtils.java
+++ app/src/main/java/com/rupalimandge/whereareyou/PermissionUtils.java
@@ -1,6 +1,7 @@
 package com.rupalimandge.whereareyou;
 
 import android.*;
+import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
@@ -11,21 +12,18 @@ import android.support.v4.content.ContextCompat;
  */
 public class PermissionUtils  {
 
-    public static void requestPermission(Context context,int requestID,String [] permissionsToBeGranted) {
+    public static void requestPermission(Context context,String permissionID,int requestID,String [] permissionsToBeGranted) {
 
         // Assume thisActivity is the current activity
 
         // Here, thisActivity is the current activity
-        /*if (ContextCompat.checkSelfPermission(context,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(context,
-                android.Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(context,
+                permissionID)
+                != PackageManager.PERMISSION_GRANTED ) {
 
             // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(context,
-                    android.Manifest.permission.ACCESS_COARSE_LOCATION)|| ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity)context,
+                    permissionID)) {
 
                 // Show an expanation to the user *asynchronously* -- don't block
                 // this thread waiting for the user's response! After the user
@@ -35,15 +33,15 @@ public class PermissionUtils  {
 
                 // No explanation needed, we can request the permission.
 
-                ActivityCompat.requestPermissions(this,
-                        new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION,},
-                        MY_PERMISSIONS_REQUEST_ACCESS_LOCATION);
+                ActivityCompat.requestPermissions((Activity)context,
+                        permissionsToBeGranted,
+                        requestID);
 
                 // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
                 // app-defined int constant. The callback method gets the
                 // result of the request.
             }
         }
-    }*/
     }
-}
+    }
+
